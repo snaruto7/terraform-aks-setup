eb85cd0996f045462ac67fe2d52d110e76f60450
Shivam Narula <shivam.narula7@gmail.com>
2020-09-06 21:52:03 +0530

    fixed rg-name

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                     = var.name
-  resource_group_name      = var.rg-name
+  resource_group_name      = var.rg_name
   location                 = var.location
   sku                      = "Standard"
   admin_enabled            = false
